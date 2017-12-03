149c201873486acb722556f9c90876216c724e68
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2017-12-03 07:40:46 -0300

    Scene Outros Jogos added.

---
+++ src/components/OutrosJogos.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+//const logo = require('./src/imgs/logo.png');
+export default class OutrosJogos extends Component {
+  render() {
+    return (
+      <Text>Testando Cena jogo</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  viewParent:{
+    flex: 1,
+    backgroundColor: '#61BD8C'
+  }
+})
